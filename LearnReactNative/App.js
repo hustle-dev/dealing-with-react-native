bb09ba0e4ebb94dfa8cdcefd84062d81d9af0595
hustle-dev <dlwoabsdk@gmail.com>
2022-09-02 22:08:32 +0900

    feat: studying chapter 1

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -52,40 +44,12 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
     </SafeAreaView>
   );
 };
--- LearnReactNative/index.js
+++ LearnReactNative/index.js
@@ -1,4 +1,9 @@
 /**
+ * 프로젝트의 엔트리 파일
+ */
+
+/**
+ * Prettier와 관련되어 있음
  * @format
  */
 
@@ -6,4 +11,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+/**
+ * @description App이라는 컴포넌트를 불러와서 이 registerComponent를 이용해 네이티브 시스템에 해당 컴포넌트를 등록한다.
+ */
 AppRegistry.registerComponent(appName, () => App);

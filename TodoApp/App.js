e690aa998aff37fa18fa3a1dbe39f42c41d1f5c9
hustle-dev <dlwoabsdk@gmail.com>
2022-09-03 22:08:49 +0900

    feat: intial setting for TodoApp

---
+++ TodoApp/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>TodoApp</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;

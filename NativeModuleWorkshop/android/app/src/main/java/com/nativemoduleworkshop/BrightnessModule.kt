105ac08707b6067ea744993ca665b94b230db503
hustle-dev <dlwoabsdk@gmail.com>
2022-09-12 15:38:55 +0900

    feat: make android native module using kotlin

---
+++ NativeModuleWorkshop/android/app/src/main/java/com/nativemoduleworkshop/BrightnessModule.kt
@@ -0,0 +1,35 @@
+package com.nativemoduleworkshop
+
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class BrightnessModule(reactContext: ReactApplicationContext): ReactContextBaseJavaModule(reactContext) {
+    override fun getName(): String {
+        return "BrightnessModule"
+    }
+
+    override fun getConstants(): MutableMap<String, Any>? {
+        val constants = HashMap<String,Any>()
+        constants.put("SAMPLE_VALUE", "Hello World")
+        return constants
+    }
+
+    @ReactMethod
+    fun getBrightness(){
+        val activity = currentActivity!!
+        val lp = activity.window.attributes
+        promise.resolve(lp.screenBrightness)
+    }
+
+    @ReactMethod
+    fun setBrightness(brightness: Float) {
+        val activity = currentActivity!!
+        activity.runOnUiThread {
+            val lp = activity.window.attributes
+            lp.screenBrightness = brightness
+            activity.window.attributes = lp
+        }
+    }
+}

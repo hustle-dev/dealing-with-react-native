5279b7dd92cff85ac3eb0840f2b74d9d8b06c274
hustle-dev <dlwoabsdk@gmail.com>
2022-09-12 16:08:14 +0900

    feat: learn typescript and make useReducer using typescript

---
+++ LearnTypeScript/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Counter from './Couter';
+import Profile from './Profile';
+
+function App() {
+  return <Counter />;
+}
+
+export default App;

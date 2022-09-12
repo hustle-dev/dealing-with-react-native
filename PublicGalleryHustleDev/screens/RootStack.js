cc3fa69df5f41e137bd9f1ebaa82c0c61d14fc98
hustle-dev <dlwoabsdk@gmail.com>
2022-09-12 10:59:43 +0900

    feat: setting default config

---
+++ PublicGalleryHustleDev/screens/RootStack.js
@@ -0,0 +1,10 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return <Stack.Navigator>{/* 화면 추가 예정 */}</Stack.Navigator>;
+}
+
+export default RootStack;
